4606c9f84ea2e6fbe80be3a7dc208660cf108c2b
ninafinageeva <154919648+ninafinageeva@users.noreply.github.com>
2024-01-22 21:41:42 +0300

    change docstring in function str_func.py

--- str_func.py
+++ str_func.py
@@ -1,12 +1,8 @@
 def text_upper(text)->str:
 	"""
-	изменить докстринг
+	тест - попытка снова
 	"""
 	return text_upper()
 
-def str_title(text)->str:
-	"""
-	Все первые буквы заглавные
-	"""
-	return text.title()
+
 
